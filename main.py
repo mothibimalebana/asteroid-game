f2206d8103a8fa3ecc3d121ef711f4bd110fc39a
mothibimalebana <mothibi.mm12@gmail.com>
2025-07-17 20:24:16 +0200

    chore: created blacksreen for the game

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
+import pygame
+from constants import *
+
 def main():
-    print("Hello from asteroid-game!")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    num = 5
+    while(num == 5):
+        screen.fill("black")
+        pygame.display.flip()
+    
 
 
 if __name__ == "__main__":
